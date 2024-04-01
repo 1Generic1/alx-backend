3658d65c23f728c276b951484e66b716f74b34d2
kelvin <kelvin4stunt@gmail.com>
2024-04-01 17:01:19 +0100

    update

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+    Calculate the start and end indices for a
+    given page in a paginated dataset.
+"""
+import csv
+from typing import List
+from math import ceil
+
+def index_range(page, page_size):
+    """
+    Calculate the start and end indices for a
+    given page in a paginated dataset.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size - 1
+    return start_index, end_index
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Calculate the start and end indices for a
+        given page in a paginated dataset.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        dataset_length = len(self.dataset())
+        total_pages = ceil(dataset_length / page_size)
+
+        if page > total_pages:
+            return []
+        start_index, end_index = index_range(page, page_size)
+        return self.dataset()[start_index:end_index+1]
