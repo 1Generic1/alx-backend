a622caa66a88b526744f2a3c4c5ebcf890586b77
kelvin <kelvin4stunt@gmail.com>
2024-04-03 22:12:10 +0100

    update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,13 +9,11 @@ class BasicCache(BaseCaching):
     """ BasicCache module
     """
 
-
     def put(self, key, item):
         """ Add an item to the cache """
         if key is None or item is None:
             return
         self.cache_data[key] = item
-
 
     def get(self, key):
         """ Get an item from the cache """
