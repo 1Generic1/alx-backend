1617379efe81e79ff8a733a4d853501f64cb1827
kelvin <kelvin4stunt@gmail.com>
2024-04-07 21:31:18 +0100

    up

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" MRUCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache inherits from BaseCaching and implements
+    a caching system using MRU eviction """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            del self.cache_data[key]
+
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            mru_key = next(reversed(self.cache_data))
+            del self.cache_data[mru_key]
+            print("DISCARD:", mru_key)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item from the cache """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
